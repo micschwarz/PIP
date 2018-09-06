d28db6876e1dd4fd6396e815d120ffdb23965d83
Michael Schwarz <089michael@gmail.com>
2018-09-06 19:23:04 +0200

    Fix codestyle

--- src/System/Controller.php
+++ src/System/Controller.php
@@ -2,37 +2,36 @@
 
 namespace System;
 
-class Controller {
+class Controller
+{
 
-	public function loadModel($name): Model
-	{
-		$model = "\\Application\\Models\\" . $name;
-
+    public function loadModel($name): Model
+    {
+        $model = "\\Application\\Models\\" . $name;
         return new $model();
-	}
+    }
 
-	public function loadView($name): View
-	{
-		return new View($name);
-	}
+    public function loadView($name): View
+    {
+        return new View($name);
+    }
 
-	public function loadPlugin($name)
-	{
+    public function loadPlugin($name)
+    {
         $plugin = "\\Application\\Plugin\\" . $name;
         return new $plugin();
-	}
+    }
 
-	public function loadHelper($name)
-	{
+    public function loadHelper($name)
+    {
         $helper = "\\Application\\Helper\\" . $name;
         return new $helper();
-	}
+    }
 
-	public function redirect($loc)
-	{
-		global $config;
-
-		header('Location: '. $config['base_url'] . $loc);
-	}
+    public function redirect($loc)
+    {
+        global $config;
+        header('Location: ' . $config['base_url'] . $loc);
+    }
 
 }
